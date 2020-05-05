1b992824451dac9740e839c0233a6012c479070c
galglogovsek <glogovsekgal@gmail.com>
2020-05-06 00:34:51 +0200

    Express & Mongoose backend

---
+++ backend/server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+//povezava z bazo
+var mongoose = require('mongoose');
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/android';
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var movieSchema = new mongoose.Schema({
+  title: String,
+  synopsis: String,
+  genre: String,
+  director: String,
+  writer: String,
+  boxoffice: String,
+  runningTimeStr: String,
+  studio: String,
+  poster: String
+});
+
+var Movie = mongoose.model('Movie', movieSchema);
+
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+});
+
+const request = require('request');
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+app.get('/movies', function(req, res) {
+  Movie.find().exec(function (err, movies) {
+    return res.json(movies);
+  });
+});
+
+app.get('/addrandom/:int', function(req, res) {
+  console.log(parseInt(req.params.int)+5);
+  for (i = req.params.int; i < parseInt(req.params.int)+5; i++) { 
+    request('https://www.rottentomatoes.com/api/private/v1.0/movies/'+i, function (error, response, body) {
+      console.error('error:', error);
+      console.log('statusCode:', response && response.statusCode);
+      var parsed = JSON.parse(body);
+      console.log(parsed.casts.directors[0].name);
+      
+      Movie.create({ title: parsed.title, synopsis: parsed.synopsis, genre: parsed.genreSet[0].displayName, director: parsed.casts.directors[0].name, writer: parsed.casts.screenwriters[0].name, boxoffice: parsed.boxoffice, runningTimeStr: parsed.runningTimeStr, studio: parsed.studio, poster: parsed.posters.detailed }, function (err, small) {
+        if (err) return handleError(err);
+      });
+      //console.log('body:', parsed);
+    });
+  } 
+});
